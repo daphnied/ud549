550be8aeb3b507a70ce470a0b3ab6bf727ea3c0a
D Donnell <daphniedonnell@gmail.com>
2018-08-29 21:05:19 -0400

    Corrected async test

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,39 @@
-describe('Address Book', function(){
-  it('should be able to add a contact', function(){
-    var addressBook = new AddressBook():
-      thisContact = new Contact();
+describe( 'AddressBook', function() {
 
-    addressBook.addContact(thisContact);
+  var ab, c;
 
-    expect(addressBook.getContact(0)).toBe(thisContact);
+  beforeEach(function() {   // beforeEach() runs before *each* testcase it()
+    ab = new AddressBook(),
+    c  = new Contact();
+  });
+
+  it('should be able to add Contact', function() {
+    ab.addContact(c);
+    expect(ab.getContact(0)).toBe(c);
+  });
+
+  it('should be able to delete a Contact', function() {
+    var ab = new AddressBook(),
+        c  = new Contact();
+    ab.addContact(c);
+    ab.deleteContact(0);
+    expect(ab.deleteContact(0)).not.toBeDefined();
+  });
+});
+
+describe('Async Address Book', function() {
+  var ab = new AddressBook();
+
+  beforeEach(function(done) {
+    ab.getInitialContacts(function() {    // getInitialContacts() is an *async* function
+      done();                             // *done() signals* to the test framework that the async func call is done
+    });
+  });
+
+  it('should grab an initial blank contact', function(done) {
+    expect(ab.initialComplete).toBe(true);
+    var c = new Contact();
+    expect(ab.getContact(0)).toEqual(c);
+    done();
   });
 });
