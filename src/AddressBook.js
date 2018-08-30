bad9e7e05f7ce6be5e6b4f72ca21159ec8b82112
D Donnell <daphniedonnell@gmail.com>
2018-08-29 20:39:43 -0400

    Added address book

---
+++ src/AddressBook.js
@@ -0,0 +1,42 @@
+function AddressBook() {
+  this.contacts = []
+  this.initialComplete = false;
+}
+
+AddressBook.prototype.addContact = function(c) {
+  this.contacts.push(c);
+}
+
+AddressBook.prototype.getContact = function(i) {
+  return this.contacts[i];
+}
+
+AddressBook.prototype.deleteContact = function(i) {
+  this.contacts.splice(i, 1);
+}
+
+AddressBook.prototype.getInitialContacts = function(cb) { // Fake async callback function
+  setTimeout(() => {
+    this.initialComplete = true; // Set to true
+    var first = new Contact();
+    this.contacts = [first];
+    if (cb) {
+      return cb();
+    }
+  }, 3);
+}
+
+AddressBook.prototype.promiseIC = function() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(() => {
+      var first = new Contact("Jordan");
+      this.contacts = [first];
+      console.log(this.contacts);
+      if (first.name != "") {
+        resolve(first);
+      } else {
+        reject(first);
+      }
+    }, 3);
+  })
+}
